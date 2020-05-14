1d7fab07fd8c4e282beadaa8359da7cd808f6339
jajanet <vjanet@umich.edu>
2020-05-14 18:10:29 -0400

    Updates

---
+++ precache-manifest.599d425bf736cbecfe19b1887cd4273f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f41e6c197f04e9c501258f342287982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75cb6b7bdcd3a0b720e7",
+    "url": "/static/css/main.a52aad26.chunk.css"
+  },
+  {
+    "revision": "a1d9654882c4c5fd0c54",
+    "url": "/static/js/2.739d380f.chunk.js"
+  },
+  {
+    "revision": "75cb6b7bdcd3a0b720e7",
+    "url": "/static/js/main.7b85be97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
