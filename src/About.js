24b70259d50ad296b0cc2b1d355f31a54df31b74
jajanet <vjanet@umich.edu>
2020-05-15 11:23:18 -0400

    add more nesting of text

--- src/About.js
+++ src/About.js
@@ -10,22 +10,36 @@ const About = () => {
           in
           <ToggleText
             before=" Michigan "
-            after=" the Grand Rapids area (Kentwood) "
+            after=" the Grand Rapids Michigan area "
           />
           studying Computer Science at University of Michigan.
+	  	</p>
+	  	<p>
+	  	  I'll be a Software Engineering Intern at Google and Confluent for
+	  	  the summer and fall of 2020 respectively.
         </p>
         <ToggleText
           hidden={
             <p>
-              As a lifelong lover of the technology, one my first endeavors on
-              the internet involved creating HTML/CSS webpages and MS Paint
-              graphics on Neopets as a creative outlet. With that, majoring in
-              computer science made the most sense for me. My recent experience
-              includes working in many different areas of technology including
-              space (CubeSats), privacy, research, data visualization,
-              education, and more. Through the course of my career, I hope to
-              explore more facets of computer science and contribute to
-              solutions that allieviate poverty and inaccessiblity.
+			  As a
+			  <ToggleText
+			  	before=" lifelong lover of technology, "
+			  	after=" lifelong lover of technology, one my first endeavors on
+			  		the internet involved creating HTML/CSS webpages and MS Paint
+			  		graphics on Neopets as a creative outlet. With that, "
+			  />
+			  majoring in computer science always made the most sense for me.
+			  My recent experience includes
+			  <ToggleText
+			  	before=" a wide range of different fields, and I hope to continue
+			  		learning a lot while making a positive impact on society"
+			  	after=" a wide range of different fields like space
+			  		(CubeSats), privacy, research, data visualization,
+			  		education, and more. Through the course of my career, I
+			  		hope to explore more facets of computer science and
+			  		contribute to solutions that allieviate poverty and
+			  		inaccessiblity"
+			  />.
               <br />
               <br />
               Outside of computers, my hobbies include: thrifting, making sushi,
