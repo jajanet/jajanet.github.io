6f864cf45ebb5d525654bb2ba6fdffac2e144558
jajanet <vjanet@umich.edu>
2020-05-14 22:00:51 -0400

    adjust button styling

--- src/Links.js
+++ src/Links.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Links = () => (
   <div className="links">
-    <div className="bg">
+    <div className="bg bg-left">
       <a href="https://me--janetvu.repl.co/res.pdf">Resume</a>
     </div>
-    <div className="bg bg2">
+    <div className="bg bg-right">
       <a href="https://drive.google.com/drive/folders/1PjdQOE-c80WCTH_7aYyjVAPUpPVOMT4b">
         design
       </a>
