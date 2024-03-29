ba87f5c8879673b44a8e53b7bd1e2b9614d30bad
Janet Vu <vjanet@umich.edu>
2024-03-28 22:26:02 -0700

    Updates

---
+++ precache-manifest.0357f07011bf855b244eeaa215a8826e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f9eb862d5a5fc1559e3929f86ddba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2755bc6d5e10be6ac21c",
+    "url": "/static/css/main.1d7911f8.chunk.css"
+  },
+  {
+    "revision": "da75ddcdda450066a628",
+    "url": "/static/js/2.a6dbc66b.chunk.js"
+  },
+  {
+    "revision": "2755bc6d5e10be6ac21c",
+    "url": "/static/js/main.0c77fe74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
